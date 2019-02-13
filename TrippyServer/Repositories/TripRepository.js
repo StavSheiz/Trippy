ad733e560d590f84c99607fff1201c1d77fb6118
DESKTOP-NLEAB5U\moria <mori.alon@gmail.com>
2019-02-13 20:55:46 +0200

    seq

--- TrippyServer/Repositories/TripRepository.js
+++ TrippyServer/Repositories/TripRepository.js
@@ -3,7 +3,7 @@ const { executeQuery } = require('../DBAccess')
 function addNewTrip(data){
     var query = `
         INSERT INTO public."TRIPS"
-        VALUES(1,$1,$2,$3,$4,$5,$6)
+        VALUES(nextval('trips_seq'),$1,$2,$3,$4,$5,$6)
     `
     const values= ['{"x": "4.6", "y":"7.8"}', '1999/09/09', '1999/09/09', "good good", [1,2], "" ];
 

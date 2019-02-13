fba96af109ba11a7a2003ce2e6e2aaca889a6058
staav sheizaaf <stav54655@gmail.com>
2019-02-13 23:37:30 +0200

    desing

--- Trippy/screens/NewTrip.js
+++ Trippy/screens/NewTrip.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { KeyboardAvoidingView ,ScrollView, View, ActivityIndicator, StyleSheet  } from 'react-native';
-import { Input, Image, Button, ThemeProvider } from 'react-native-elements';
+import { Input, Image, Button, ThemeProvider, Divider } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { TopNavigation } from '../components/TopNavigation/TopNavigation'
 
 export default class NewTrip extends React.Component {
+    static navigationOptions = {
+		header: null,
+	};
     constructor(props) {
         super();
 
@@ -14,7 +18,8 @@ export default class NewTrip extends React.Component {
             Location: '',
             StartDate: '',
             EndDate: '',
-            Details: ''
+            Details: '',
+            listViewDisplayed: false
         }
     }
 
@@ -27,22 +32,24 @@ export default class NewTrip extends React.Component {
 
         return (
         
-        <ThemeProvider theme={theme}>
+        <View style={styles.mainView}>
+        	<TopNavigation title="Let's Travel"></TopNavigation>
             <KeyboardAwareScrollView>
-                <View keyboardShouldPersistTaps='handled'>   
+                <View keyboardShouldPersistTaps='handled' >   
                     <Image
                         source={{ uri: imgUrl }}
                         style={{ height: 200 }}
                         PlaceholderContent={<ActivityIndicator />}
                     />
                     <Input containerStyle={styles.row} value={this.state.Name} placeholder='Trip Name' onChange={this.handleTextChange.bind(this, 'Name')}/>
+                    <Divider  style={{ backgroundColor: '#f8bbd0' }}/>
                     <View style={styles.row}>
                         <GooglePlacesAutocomplete
-                            placeholder='Search'
+                            placeholder='Location'
                             minLength={2} 
                             autoFocus={false}
                             returnKeyType={'search'} 
-                            listViewDisplayed='auto'   
+                            listViewDisplayed={this.state.listViewDisplayed}   
                             fetchDetails={true}
                             renderDescription={row => row.description} 
                             onPress={(data, details = null) => { 
@@ -50,45 +57,37 @@ export default class NewTrip extends React.Component {
                                     Location: data.description
                                 });
                             }}
-                            
+                            styles={{    
+                                textInputContainer: {
+                                backgroundColor: '#f6f5f3',
+                                borderTopWidth: 0,
+                                borderBottomWidth:0
+                              },
+                              textInput: {
+                                marginLeft: 0,
+                                marginRight: 0,
+                                // height: 38,
+                                color: '#5d5d5d',
+                                fontSize: 16,
+                                borderBottomColor: 'black',
+                                borderBottomWidth: 1
+                              },
+                            //   separator: {
+                            //       backgroundColor: 'white'
+                            //   },
+                              container: {
+                                  zIndex: 99
+                              },
+                              listView: {
+                                  zIndex: 99999
+                              }
+                            }}
                             getDefaultValue={() => ''}
                             
                             query={{                 
                                 key: 'AIzaSyDe2cx9NLqtDipMKZ1J2EeioMAn2W9L_20',
                                 language: 'en', 
                                 types: '(cities)'
-                            }}
-                            
-                            // styles={{
-                            //     textInputContainer: {
-                            //         width: '100%'
-                            //     },
-                            //     description: {
-                            //         fontWeight: 'bold'
-                            //     },
-                            //     predefinedPlacesDescription: {
-                            //         color: '#1faadb'
-                            //     },
-                            //     listView: {
-                            //         backgroundColor: '#ffffff',
-                            //         color: 'black',
-                            //         position: 'absolute',
-                            //         zIndex: 99999,
-                            //         top: 30
-                            //     } 
-                            // }}
-
-                            styles={{
-                                description: {
-                                    fontWeight: "bold"
-                                },
-                                listView: {
-                                    backgroundColor: '#ffffff',
-                                    color: 'black',
-                                    position: 'absolute',
-                                    zIndex: 99999,
-                                    top: 40
-                                } 
                             }}
                             
                             enablePoweredByContainer={true}
@@ -103,7 +102,7 @@ export default class NewTrip extends React.Component {
                             debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
                         />
                     </View>
-                    
+                    <Divider  style={{ backgroundColor: '#f8bbd0' }}/>
                     <View style={styles.DatePickers}>
                         <DatePicker
                             style={styles.DatePicker}
@@ -117,7 +116,6 @@ export default class NewTrip extends React.Component {
                             cancelBtnText="Cancel"
                             customStyles={{
                                 dateInput: {
-                                    marginLeft: 35
                                 }
                             }}
                             onDateChange={(date) => {this.setState({StartDate: date})}}
@@ -134,42 +132,42 @@ export default class NewTrip extends React.Component {
                             cancelBtnText="Cancel"
                             customStyles={{
                                 dateInput: {
-                                    marginLeft: 35
                                 }
                             }}
                             onDateChange={(date) => {this.setState({EndDate: date})}}
                         />
                     </View>
+                    <Divider  style={{ backgroundColor: '#f8bbd0' }}/>
+
                     <Input containerStyle={styles.row} value={this.state.Details} placeholder='My ideal partner' onChange={this.handleTextChange.bind(this, 'Details')}/>
                 </View>
             </KeyboardAwareScrollView>
-        </ThemeProvider>
+        </View>
         )
     }
 }
 
 
 const theme = {
-    Input: {
-      backgroundColor: '',
-    }
+
   };
 
 const styles = StyleSheet.create({
     DatePickers: {
-        height: 70,
+        paddingTop:20,
+        paddingBottom:20,
         flexDirection: "row"
     },
     DatePicker: {
-        width: 180
+        width: 180,
+        paddingLeft: 10
     },
     row: {
-        height: 70,
-        zIndex: 1
-        // marginTop: 3,
+        padding: 10
     },
     mainView: {
         flexGrow: 1,
-        flexDirection: "column"
+        flexDirection: "column",
+        backgroundColor: '#f6f5f3'
     }
 });
--- Trippy/services/serverRequests.js
+++ Trippy/services/serverRequests.js
@@ -8,9 +8,18 @@ export function addTags(user, trip, tags) {
         .then((response) => response.json())
 }
 
-export function getTags() {
-    return axios.get('https://localhost:3000/getInterests')
-}
-
+// export function getTags() {
+//     return axios.get('https://localhost:3000/getInterests').then(()=>{},(err)=>{console.log(err)})
+// }
+export async function getTags() {
+    try {
+      console.log('send')
+      return axios.get(
+        'https://192.168.43.244:3000/getIntrests'
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
 export function addPartner() {}
 export function nextPartner() {}

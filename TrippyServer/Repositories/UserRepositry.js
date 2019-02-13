1e9549fffea7a1e951910ed2d74d1066deeae034
DESKTOP-NLEAB5U\moria <mori.alon@gmail.com>
2019-02-13 21:44:06 +0200

    get trips for user

---
+++ TrippyServer/Repositories/UserRepositry.js
@@ -0,0 +1,16 @@
+const { executeQuery } = require('../DBAccess')
+
+function getTripsForUser(data){
+    let query =`
+        SELECT t."ID", t."LOCATION", t."START_DATE", t."END_DATE", t."DETAILS", t."INTRESTS", t."IMG", tu."IS_OWNER"
+        FROM public."TRIP_USER" tu,
+            public."TRIPS" t
+        WHERE tu."TRIP_ID" = t."ID" AND
+              tu."USER_ID" = $1
+    `
+    const values=[1000]
+
+    return executeQuery(query, values)
+}
+
+module.exports = {getTripsForUser};

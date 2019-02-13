a07ddf7f2a5beabd644d04452feb9b911ffacec3
czimer <czimer93@gmail.com>
2019-02-13 16:05:59 +0200

    new trip continue

--- Trippy/screens/NewTrip.js
+++ Trippy/screens/NewTrip.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import { View, ActivityIndicator  } from 'react-native';
-import { Input, Image } from 'react-native-elements';
+import { View, ActivityIndicator, StyleSheet  } from 'react-native';
+import { Input, Image, Button, ThemeProvider } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
-import {I18nManager} from 'react-native';
-import { ThemeProvider } from 'react-native-elements';
-I18nManager.allowRTL(false);
-
-const theme = {
-  Input: {
-    backgroundColor: '',
-  }
-};
 
 export default class NewTrip extends React.Component {
     constructor(props) {
         super();
 
         this.state = {
-            Name: 'שביל הגולן',
+            Name: '',
             Location: '',
             StartDate: '',
             EndDate: '',
@@ -36,51 +27,78 @@ export default class NewTrip extends React.Component {
         return (
         
         <ThemeProvider theme={theme}>
-            <View>   
+            <View style={styles.mainView}>   
                 <Image
                     source={{ uri: imgUrl }}
                     style={{ height: 200 }}
                     PlaceholderContent={<ActivityIndicator />}
                 />
-                <Input value={this.state.Name} placeholder='Trip Name' onChange={this.handleTextChange.bind(this, 'Name')}/>
-                <Input value={this.state.Location} placeholder='Location' onChange={this.handleTextChange.bind(this, 'Location')}/>
-                <DatePicker
-                    style={{width: 200}}
-                    date={this.state.StartDate}
-                    mode="date"
-                    placeholder="Start Date"
-                    format="YYYY-MM-DD"
-                    minDate="2016-05-01"
-                    maxDate="2016-06-01"
-                    confirmBtnText="Confirm"
-                    cancelBtnText="Cancel"
-                    customStyles={{
-                        dateInput: {
-                            marginLeft: 36
-                        }
-                    }}
-                    onDateChange={(date) => {this.setState({StartDate: date})}}
-                />
-                <DatePicker
-                    style={{width: 200}}
-                    date={this.state.EndDate}
-                    mode="date"
-                    placeholder="End Date"
-                    format="YYYY-MM-DD"
-                    minDate="2016-05-01"
-                    maxDate="2016-06-01"
-                    confirmBtnText="Confirm"
-                    cancelBtnText="Cancel"
-                    customStyles={{
-                        dateInput: {
-                            marginLeft: 36
-                        }
-                    }}
-                    onDateChange={(date) => {this.setState({EndDate: date})}}
-                />
-                <Input value={this.state.Details} multiline placeholder='My ideal partner' onChange={this.handleTextChange.bind(this, 'Details')}/>
+                <Input containerStyle={styles.row} value={this.state.Name} placeholder='Trip Name' onChange={this.handleTextChange.bind(this, 'Name')}/>
+                <Input containerStyle={styles.row} value={this.state.Location} placeholder='Location' onChange={this.handleTextChange.bind(this, 'Location')}/>
+                <View style={styles.DatePickers}>
+                    <DatePicker
+                        style={styles.DatePicker}
+                        date={this.state.StartDate}
+                        mode="date"
+                        placeholder="Start Date"
+                        format="YYYY-MM-DD"
+                        minDate="2016-05-01"
+                        maxDate="2016-06-01"
+                        confirmBtnText="Confirm"
+                        cancelBtnText="Cancel"
+                        customStyles={{
+                            dateInput: {
+                                marginLeft: 35
+                            }
+                        }}
+                        onDateChange={(date) => {this.setState({StartDate: date})}}
+                    />
+                    <DatePicker
+                        style={styles.DatePicker}
+                        date={this.state.EndDate}
+                        mode="date"
+                        placeholder="End Date"
+                        format="YYYY-MM-DD"
+                        minDate="2016-05-01"
+                        maxDate="2016-06-01"
+                        confirmBtnText="Confirm"
+                        cancelBtnText="Cancel"
+                        customStyles={{
+                            dateInput: {
+                                marginLeft: 35
+                            }
+                        }}
+                        onDateChange={(date) => {this.setState({EndDate: date})}}
+                    />
+                </View>
+                <Input containerStyle={styles.row} value={this.state.Details} placeholder='My ideal partner' onChange={this.handleTextChange.bind(this, 'Details')}/>
             </View>
         </ThemeProvider>
         )
     }
 }
+
+
+const theme = {
+    Input: {
+      backgroundColor: '',
+    }
+  };
+
+const styles = StyleSheet.create({
+    DatePickers: {
+        height: 70,
+        flexDirection: "row"
+    },
+    DatePicker: {
+        width: 180
+    },
+    row: {
+        height: 70,
+        // marginTop: 3,
+    },
+    mainView: {
+        flex: 1,
+        flexDirection: "column"
+    }
+});

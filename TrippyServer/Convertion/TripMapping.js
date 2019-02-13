21fad588e4e09e742a89564bae5a0fdf734ab259
diklaYoni <diklooly@gmail.com>
2019-02-13 22:41:22 +0200

    fix

--- TrippyServer/Convertion/TripMapping.js
+++ TrippyServer/Convertion/TripMapping.js
@@ -1,4 +1,4 @@
-const mapTripUserssFromDBToObject = function(tripList) {
+const mapTripUsersFromDBToObject = function(tripList) {
 
     var convertedList = [];
 
@@ -15,6 +15,24 @@ const mapTripUserssFromDBToObject = function(tripList) {
     }
 
 }
+
+const mapTripsFromDBToObject = function(tripList) {
+    
+        var convertedList = [];
+    
+        for(var i in tripList){
+            currTrip = tripList[i];
+    
+            var convertedTrip = {
+                TripId: currTrip["TRIP_ID"],
+                UserId: currTrip["USER_ID"],
+                IsOwner: currTrip["IS_OWNER"]
+            };
+    
+            convertedList.push(convertedList);
+        }
+    
+    }
 
 module.exports = {
     mapTripUserssFromDBToObject:mapTripUserssFromDBToObject
